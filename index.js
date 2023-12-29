a2f0c94d3434efb27001b47ca4763eed3b0713f6
muhumbania <franklinmuhumbania@gmail.com>
2023-12-29 18:54:26 +0300

    Rendered the posts on the home page

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import bodyParser from "body-parser";
 import express from "express";
 
 const app = express();
@@ -5,8 +6,23 @@ const port = 3000;
 
 app.use(express.static('public'));
 
+const postList = [{
+    title: "This is the title 1",
+    body: "Exploring the vast landscape of technology, we find ourselves immersed in a world of constant innovation. From the latest advancements in artificial intelligence to the intricacies of web development, the journey is both exciting and challenging. In this blog post, we delve into the realm of emerging technologies, shedding light on their impact on our daily lives. Join us on this exploration as we navigate through the ever-evolving digital landscape, uncovering the stories that shape the future of technology"
+}, 
+{
+    title: "This is the  2",
+    body: "Exploring the vast landscape of technology, we find ourselves immersed in a world of constant innovation. From the latest advancements in artificial intelligence to the intricacies of web development, the journey is both exciting and challenging. In this blog post, we delve into the realm of emerging technologies, shedding light on their impact on our daily lives. Join us on this exploration as we navigate through the ever-evolving digital landscape, uncovering the stories that shape the future of technology"
+},
+{
+    title: "This is the title 3",
+    body: "Exploring the vast landscape of technology, we find ourselves immersed in a world of constant innovation. From the latest advancements in artificial intelligence to the intricacies of web development, the journey is both exciting and challenging. In this blog post, we delve into the realm of emerging technologies, shedding light on their impact on our daily lives. Join us on this exploration as we navigate through the ever-evolving digital landscape, uncovering the stories that shape the future of technology"
+}
+]
+
+
 app.get('/', (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {posts : postList});
 });
 
 app.listen(port, ()=>{
